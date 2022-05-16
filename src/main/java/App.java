d2a77467548bb15598b2507638300c29e5211e30
zmo00 <zmo00@yandex.ru>
2022-05-16 11:23:04 +0300

    Result0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld beanHelloWorld1 = (HelloWorld) context.getBean("helloworld");
+        HelloWorld beanHelloWorld2 = context.getBean("helloworld", HelloWorld.class);
+
+        Cat beanCat1 = context.getBean("cat", Cat.class);
+        Cat beanCat2 = context.getBean("cat", Cat.class);
+
+        System.out.println(beanHelloWorld1 == beanHelloWorld2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
